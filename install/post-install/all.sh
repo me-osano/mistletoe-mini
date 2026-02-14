79b2e22fa01268d9c58393c1160c1854ffa410d8
Enosh Misonge Osano <osanoenosh@gmail.com>
2026-02-15 00:01:11 +0300

    postinstall

--- install/post-install/all.sh
+++ install/post-install/all.sh
@@ -1,2 +1,5 @@
-run_logged $MISTLETOE_INSTALL/post-install/pacman.sh
+source $MISTLETOE_INSTALL/post-install/battery-monitor.sh
+source $MISTLETOE_INSTALL/post-install/dns-resolver.sh
+source $MISTLETOE_INSTALL/post-install/elephant.sh
+source $MISTLETOE_INSTALL/post-install/firewall.sh
 source $MISTLETOE_INSTALL/post-install/finished.sh
--- install/post-install/pacman.sh
+++
@@ -1,3 +0,0 @@
-# Configure pacman
-sudo cp -f ~/.local/share/mistletoe/default/pacman/pacman.conf /etc/pacman.conf
-sudo cp -f ~/.local/share/mistletoe/default/pacman/mirrorlist /etc/pacman.d/mirrorlist
